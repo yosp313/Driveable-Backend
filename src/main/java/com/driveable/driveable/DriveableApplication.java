7c040ec3a2a5b7fafb77d65fa7f38b4b6781d836
Youssef Ayman <youssef.ayman@robustastudio.com>
2024-12-09 13:54:36 +0200

    init

---
+++ src/main/java/com/driveable/driveable/DriveableApplication.java
@@ -0,0 +1,13 @@
+package com.driveable.driveable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveableApplication.class, args);
+    }
+
+}
