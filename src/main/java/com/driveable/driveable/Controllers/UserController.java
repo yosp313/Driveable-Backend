719428862052314796d494583bf37c194a7e4eaf
Youssef Ayman <youssef.ayman@robustastudio.com>
2024-12-15 20:35:22 +0200

    feat: implemented the rest of the routes in the user controller

--- src/main/java/com/driveable/driveable/Controllers/UserController.java
+++ src/main/java/com/driveable/driveable/Controllers/UserController.java
@@ -2,6 +2,7 @@ package com.driveable.driveable.Controllers;
 
 import com.driveable.driveable.Models.User;
 import com.driveable.driveable.Services.UserService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +28,27 @@ public class UserController {
     public ResponseEntity<User> CreateUser(@RequestBody User user){
         User createdUser = userService.createUser(user);
 
-        return ResponseEntity.ok(createdUser);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> GetUserById(@PathVariable Long id){
+        User user = userService.findUserById(id);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/name/{id}")
+    public ResponseEntity<User> UpdateUserName(@PathVariable Long id, @RequestParam String firstName, @RequestParam String lastName){
+        User user = userService.updateUserName(id, firstName, lastName);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> DeleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/driveable/driveable/Services/UserService.java
+++ src/main/java/com/driveable/driveable/Services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,6 +22,29 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        userRepository.save(user);
+
+        return user;
+    }
+
+    public void deleteUser(Long id) {
+        User user = findUserById(id);
+
+        userRepository.delete(user);
+    }
+
+    public User findUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        return user.orElse(null);
+    }
+
+    public User updateUserName(Long id, String firstName, String lastName) {
+        User user = findUserById(id);
+
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+
         userRepository.save(user);
 
         return user;
