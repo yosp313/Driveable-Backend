3dcf64173b0f1ff2663cce91a0c5ea154b8f22ef
Youssef Ayman <yosp31@gmail.com>
2025-02-16 17:21:54 +0200

    docs: added a readme.md and added todo's

--- src/main/java/com/driveable/driveable/Controllers/AdminDashboardController.java
+++ src/main/java/com/driveable/driveable/Controllers/AdminDashboardController.java
@@ -4,7 +4,6 @@ import com.driveable.driveable.Models.User;
 import com.driveable.driveable.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,36 +11,37 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/admin-dashboard/users")
 public class AdminDashboardController {
-    private final UserService userService;
+  private final UserService userService;
 
-    @Autowired
-    public AdminDashboardController(UserService userService) {
-        this.userService = userService;
-    }
+  @Autowired
+  public AdminDashboardController(UserService userService) {
+    this.userService = userService;
+  }
 
-    @GetMapping
-    public List<User> GetAllUsers(){
-        return userService.findAll();
-    }
+  @GetMapping
+  public List<User> GetAllUsers() {
+    return userService.findAll();
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<User> GetUserById(@PathVariable Long id){
-        User user = userService.findUserById(id);
+  @GetMapping("/{id}")
+  public ResponseEntity<User> GetUserById(@PathVariable Long id) {
+    User user = userService.findUserById(id);
 
-        return ResponseEntity.ok(user);
-    }
+    return ResponseEntity.ok(user);
+  }
 
-    @PutMapping("/name/{id}")
-    public ResponseEntity<User> UpdateUserName(@PathVariable Long id, @RequestParam String firstName, @RequestParam String lastName){
-        User user = userService.updateUserName(id, firstName, lastName);
+  @PutMapping("/name/{id}")
+  public ResponseEntity<User> UpdateUserName(@PathVariable Long id, @RequestParam String firstName,
+      @RequestParam String lastName) {
+    User user = userService.updateUserName(id, firstName, lastName);
 
-        return ResponseEntity.ok(user);
-    }
+    return ResponseEntity.ok(user);
+  }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<User> DeleteUser(@PathVariable Long id){
-        userService.deleteUser(id);
+  @DeleteMapping("/{id}")
+  public ResponseEntity<User> DeleteUser(@PathVariable Long id) {
+    userService.deleteUser(id);
 
-        return ResponseEntity.noContent().build();
-    }
+    return ResponseEntity.noContent().build();
+  }
 }
